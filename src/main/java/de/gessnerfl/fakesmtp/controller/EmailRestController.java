2469a2162ca57719af27a8729e3f0c1aa45d523d
GyeonghoYeo2 <g_yeo@yumemi.co.jp>
2017-08-07 14:06:12 +0900

    Fixed endpoint path because URL path can fail with dot mark

--- src/main/java/de/gessnerfl/fakesmtp/controller/EmailRestController.java
+++ src/main/java/de/gessnerfl/fakesmtp/controller/EmailRestController.java
@@ -1,31 +1,32 @@
 package de.gessnerfl.fakesmtp.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import de.gessnerfl.fakesmtp.model.Email;
 import de.gessnerfl.fakesmtp.repository.EmailRepository;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 @Controller
 public class EmailRestController {
     private final EmailRepository emailRepository;
+    private final Logger logger;
 
     @Autowired
-    public EmailRestController(EmailRepository emailRepository) {
+    public EmailRestController(EmailRepository emailRepository, Logger logger) {
         this.emailRepository = emailRepository;
+        this.logger = logger;
     }
 
     public Map<String, String> parseIntoMap(Email email) {
@@ -42,9 +43,11 @@ public class EmailRestController {
         return emailMap;
     }
 
-    @RequestMapping(path = "/rest/email/{messageId}", method = org.springframework.web.bind.annotation.RequestMethod.GET, produces = "application/json")
+    @RequestMapping(path = "/rest/email", method = org.springframework.web.bind.annotation.RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public Map<String, String> getEmailById(@PathVariable String messageId, HttpServletResponse response) throws JsonProcessingException {
+    public Map<String, String> getEmailById(HttpServletRequest request, HttpServletResponse response) throws JsonProcessingException {
+        String messageId = request.getParameter("messageId");
+        logger.info("requested messageId::" + messageId);
         Email email = emailRepository.findByMessageId(messageId);
 
         Map<String,String> emailJson = new HashMap<String,String>();
@@ -59,11 +62,10 @@ public class EmailRestController {
         return emailJson;
     }
 
-    @RequestMapping(path = "/rest/email/}", method = org.springframework.web.bind.annotation.RequestMethod.GET, produces = "application/json")
+    @RequestMapping(path = "/rest/emails", method = org.springframework.web.bind.annotation.RequestMethod.GET, produces = "application/json")
     @ResponseBody
-    public List<Map<String, String>> getEmailById(HttpServletResponse response) throws JsonProcessingException {
+    public List<Map<String, String>> getEmails(HttpServletResponse response) throws JsonProcessingException {
         List<Email> emails = emailRepository.findEmails();
-
         return emails.stream().map(email -> parseIntoMap(email)).collect(Collectors.toList());
     }
 
